279e508ee41d228b1b9f848d8b5cc33090d88552
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-07-29 00:08:33 +0800

    modify index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   UrlList.find()
     .lean()
     .then(Url => {
-      const inputUrlList = Url.filter(url => url.inputUrl.length !== 0)
+      const inputUrlList = Url.filter(url => url.inputUrl.length !== 0)  
       res.render('index', { inputUrlList })
     })
     .catch(error => console.log(error))
@@ -25,9 +25,6 @@ router.post('/', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
     })
-  // UrlList.updateOne({ inputUrl: ""}, { $set:{ 'inputUrl': inputUrl }})
-  //   .then(() => res.redirect('/'))
-  //   .catch(error => console.log(error))
 })
 
 router.get('/:id', (req, res) => {
